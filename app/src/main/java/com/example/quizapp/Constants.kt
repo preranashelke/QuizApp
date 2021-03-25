3e017397ac52c3b59a785844f9b816ea33d0a00a
nehak2209 <nehak2209pc@gmail.com>
2021-03-25 09:28:12 +0530

    sports questions added and the app is working

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -9,100 +9,100 @@ object Constants {
     fun getQuestions():ArrayList<Question>{
         val questionsList=ArrayList<Question>()
         val que1 = Question(
-            1, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_argentina,
-            "Argentina", "Australia",
-            "Armenia", "Austria", 1
+            1, "What is the name of this sport ?",
+            R.drawable.volleyball,
+            "Volleyball", "Archery",
+            "Rugby", "Football", 1
         )
 
         questionsList.add(que1)
 
         // 2
         val que2 = Question(
-            2, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_australia,
-            "Angola", "Austria",
-            "Australia", "Armenia", 3
+            2, "What is the name of this sport ?",
+            R.drawable.tennis_1,
+            "Tennis", "Badminton",
+            "Archery", "Skating", 1
         )
 
         questionsList.add(que2)
 
         // 3
         val que3 = Question(
-            3, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_brazil,
-            "Belarus", "Belize",
-            "Brunei", "Brazil", 4
+            3, "What is the name of this sport ?",
+            R.drawable.swimming,
+            "Archery", "Swimming",
+            "Running", "Cricket", 2
         )
 
         questionsList.add(que3)
 
         // 4
         val que4 = Question(
-            4, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_belgium,
-            "Bahamas", "Belgium",
-            "Barbados", "Belize", 2
+            4, "What is the name of this sport ?",
+            R.drawable.running,
+            "Running", "Throw Ball",
+            "Archery", "None Of These", 1
         )
 
         questionsList.add(que4)
 
         // 5
         val que5 = Question(
-            5, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_fiji,
-            "Gabon", "France",
-            "Fiji", "Finland", 3
+            5, "What is the name of this sport ?",
+            R.drawable.rugby,
+            "Rugby", "Volleyball",
+            "Swimming", "Football", 1
         )
 
         questionsList.add(que5)
 
         // 6
         val que6 = Question(
-            6, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_germany,
-            "Germany", "Georgia",
-            "Greece", "none of these", 1
+            6, "What is the name of this sport ?",
+            R.drawable.football,
+            "Rugby", "Archery",
+            "Football", "none of these", 3
         )
 
         questionsList.add(que6)
 
         // 7
         val que7 = Question(
-            7, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_denmark,
-            "Dominica", "Egypt",
-            "Denmark", "Ethiopia", 3
+            7, "What is the name of this sport ?",
+            R.drawable.cricket,
+            "Cricket", "Baseball",
+            "Football", "WeightLifting", 1
         )
 
         questionsList.add(que7)
 
         // 8
         val que8 = Question(
-            8, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_india,
-            "Ireland", "Iran",
-            "Hungary", "India", 4
+            8, "What is the name of this sport ?",
+            R.drawable.baseball,
+            "Volleyball", "Cricket",
+            "Swimming", "Baseball", 4
         )
 
         questionsList.add(que8)
 
         // 9
         val que9 = Question(
-            9, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_new_zealand,
-            "Australia", "New Zealand",
-            "Tuvalu", "United States of America", 2
+            9, "What is the name of this sport ?",
+            R.drawable.badminton,
+            "Cricket", "Badminton",
+            "Archery", "Tennis", 2
         )
 
         questionsList.add(que9)
 
         // 10
         val que10 = Question(
-            10, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_kuwait,
-            "Kuwait", "Jordan",
-            "Sudan", "Palestine", 1
+            10, "What is the name of this sport ?",
+            R.drawable.archery1,
+            "Football", "Cricket",
+            "Archery", "Tennis", 3
         )
 
         questionsList.add(que10)
